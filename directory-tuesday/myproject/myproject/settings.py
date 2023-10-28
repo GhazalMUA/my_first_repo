7eb0e486b14419fdb22825b84230198c8cc73c25
ghazalmua <hafezibirganighazal@gmail.com>
2023-10-28 18:30:56 +0330

    template

--- directory-tuesday/myproject/myproject/settings.py
+++ directory-tuesday/myproject/myproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,12 +75,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {   
+    'default': {   
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'mydatabase',   
+        'USER': 'root',   
+        'PASSWORD': '8384acdb',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        }   
+    }   
+} 
+
+
 
 
 # Password validation
--- directory-tuesday/myproject/myproject/urls.py
+++ directory-tuesday/myproject/myproject/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from myapp import views
 
 urlpatterns = [
@@ -7,4 +7,6 @@ urlpatterns = [
     path('information/', views.ghazaview),
     path('home/', views.form_view),
     path('shoma/', views.formedo_view),
+    path('' , include('myapp.urls')),
+
 ]
