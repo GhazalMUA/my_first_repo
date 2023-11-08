4ad3e047ca3094e9ed35fbd46e666309706b980a
ghazalmua <hafezibirganighazal@gmail.com>
2023-11-08 15:47:46 +0330

    8november.2

--- LittleLemon/LittleLemonDRF/models.py
+++ LittleLemon/LittleLemonDRF/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class MenuItem(models.Model):
     title=models.CharField(max_length=200)
-    price=models.DecimalField(decimal_places=2 , max_digits=2)
+    price=models.DecimalField(decimal_places=2 , max_digits=10)
     inventory= models.SmallIntegerField(default=0)
+
+    def __str__(self):
+     return (self.title , self.price)
+ 
